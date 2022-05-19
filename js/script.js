ee7d7844d7f928fac2e79f1f7d43d36b4c2f1640
moretti-matteo <matteomoretti2000@hotmail.it>
2022-05-19 14:19:06 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message:"hello world",
+        image:"https://images.unsplash.com/photo-1652808104236-2d9170d3f484?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"
+    },
+    methods: {
+
+    }
+});
